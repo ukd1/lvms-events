63a068b8189454a5d8b38f5417fe73f17c9ff936
Russell Smith <ukd1@users.noreply.github.com>
2022-10-17 20:33:39 -0700

    Try doing longer events smarter

--- main.rb
+++ main.rb
@@ -15,11 +15,15 @@ end
 
 puts "BEGIN:VCALENDAR"
 puts "NAME:LVMS Events"
+puts "VERSION:2.0"
+puts "PRODID:https://github.com/ukd1/lvms-events"
 puts "DESCRIPTION:Las Vegas Motor Speedway Events, parsed from https://www.lvms.com/events/. Code @ https://github.com/ukd1/lvms-events."
 puts "UID:https://github.com/ukd1/lvms-events"
 puts "URL:https://github.com/ukd1/lvms-events"
-puts "SOURCE;VALUE=URI:https://raw.githubusercontent.com/ukd1/lvms-events/lvms-events.ics"
+puts "SOURCE;VALUE=URI:https://raw.githubusercontent.com/ukd1/lvms-events/main/lvms-events.ics"
 puts "IMAGE;VALUE=URI;DISPLAY=BADGE;FMTTYPE=image/png:https://www.lvms.com/images/header-lvms1.png"
+
+event_by_id = {}
 
 JSON.parse("{" + events_js.text.strip.split("\n")[1].strip.gsub('\'', '"') + "}").each do |date, event|
     doc = Nokogiri::HTML(event)
@@ -33,18 +37,31 @@ JSON.parse("{" + events_js.text.strip.split("\n")[1].strip.gsub('\'', '"') + "}"
     dtstart = Date.strptime(link.attribute('data-date')&.value, '%m/%d/%Y')
     dtend = dtstart+1
 
-    # puts
+    event_by_id[id] ||= {
+        url: url,
+        name: name,
+        id: id,
+        dtstart: dtstart,
+        dtend: dtend,
+    }
+
+    event_by_id[id][:dtend] = dtend if event_by_id[id][:dtend] < dtend
+end
+
+event_by_id.each do |id, event|
     puts "BEGIN:VEVENT"
     puts "UID:LLVMS-#{id}"
-    puts "SUMMARY:#{name}"
-    puts "URL:#{url}"
-    puts "DTSTART;VALUE=DATE:#{"%04d%02d%02d" % [dtstart.year, dtstart.month, dtstart.day]}"
-    puts "DTEND;VALUE=DATE:#{"%04d%02d%02d" % [dtend.year, dtend.month, dtend.day]}"
+    puts "SUMMARY:#{event[:name]}"
+    puts "URL:#{event[:url]}"
+    puts "LOCATION:Las Vegas Motor Speedway, 7000 Las Vegas Blvd. North, Las Vegas, NV 89115, USA"
+
+    # https://www.kanzaki.com/docs/ical/dtstamp.html - apparently required
+    puts "DTSTAMP:#{"%04d%02d%02d" % [event[:dtstart].year, event[:dtstart].month, event[:dtstart].day]}T000000Z"
+    puts "DTSTART;VALUE=DATE:#{"%04d%02d%02d" % [event[:dtstart].year, event[:dtstart].month, event[:dtstart].day]}"
+    puts "DTEND;VALUE=DATE:#{"%04d%02d%02d" % [event[:dtend].year, event[:dtend].month, event[:dtend].day]}"
     puts "ORGANIZER;CN=LVMS:MAILTO:ticketservices@lvms.com"
     puts "GEO:36.2724;-115.0104"
     puts "END:VEVENT"
-    # puts "\t\t#{event}"
 end
-
 
 puts "END:VCALENDAR"
